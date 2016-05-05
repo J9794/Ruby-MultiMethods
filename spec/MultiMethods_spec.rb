6cd3d99bef3500998fbb670c04d2cdc43265a761
j9794 <juanmanuel9794@hotmail.com>
2016-05-05 18:20:25 -0300

    Keeping the tests DRY

--- spec/MultiMethods_spec.rb
+++ spec/MultiMethods_spec.rb
@@ -1,6 +1,84 @@
 require 'rspec'
 require_relative '../src/MultiMethods'
 
+shared_context 'Multimethods Behavior' do |receiver_instance, receiver_class, multi_methods_list|
+
+  describe 'Calling the methods' do
+
+    describe 'Methods that does not use self' do
+
+      it 'should choose the correct and closest method to execute' do
+        expect(receiver_instance.concat('hello', ' world')).to eq 'hello world'
+      end
+
+      it 'should choose the correct and closest method to execute' do
+        expect(receiver_instance.concat('hello', 3) ).to eq 'hellohellohello'
+      end
+
+      it 'should choose the correct and closest method to execute' do
+        expect(receiver_instance.concat(['hello', ' world', '!'])).to eq 'hello world!'
+      end
+
+      it 'should choose the correct and closest method to execute' do
+        expect(receiver_instance.concat(Object.new, 3)).to eq 'Objetos concatenados'
+      end
+
+      it 'should throw error if no compatible definition is available' do
+        expect{receiver_instance.concat('hello', 'world', '!')}.to raise_error ArgumentError
+      end
+
+    end
+
+
+    describe 'Methods that use self' do
+
+    end
+
+  end
+
+  describe 'Asking for the multimethods' do
+
+    it 'should return one symbol representing the multimethod' do
+      expect(receiver_class.multimethods).to include *multi_methods_list
+    end
+    it 'should return the array of partialblocks which represents the multimethod' do
+      expect(receiver_class.multimethod(:concat).is_a? Array).to be true
+    end
+    it 'should be true that all of the elements on the array are partialblocks' do
+      expect(receiver_class.multimethod(:concat).all? do |mm| mm.is_a? PartialBlock end).to be true
+    end
+  end
+
+  describe 'respond_to?' do
+
+    it 'should respond if the method is a known multimethod' do
+      expect(receiver_instance.respond_to?(:concat)).to be true
+    end
+
+    it 'should respond if the method is a known regular method' do
+      expect(receiver_instance.respond_to?(:to_s)).to be true
+    end
+
+    it 'should respond if the method is a known multimethod and the types are correct' do
+      expect(receiver_instance.respond_to?(:concat, false, [String,String])).to be true
+    end
+
+    it 'should respond if the method is a known multimethod and the types are correct' do
+      expect(receiver_instance.respond_to?(:concat, false, [Integer,A])).to be true
+    end
+
+    it 'should not respond if the method is a regular method and types are provided' do
+      expect(receiver_instance.respond_to?(:to_s, false, [String]) ).to be false
+    end
+
+    it 'should not respond if the method is a multimethod but the types are not correct' do
+      expect(receiver_instance.respond_to?(:concat, false, [String,String,String])).to be false
+    end
+
+  end
+
+end
+
 describe 'MultiMethods' do
 
   describe 'MultiMethods of a class' do
@@ -25,72 +103,8 @@ describe 'MultiMethods' do
 
       end
 
-      describe 'Calling the methods' do
+      include_context 'Multimethods Behavior',A.new,A,[:respond_to?,:concat]
 
-
-        it 'should choose the correct and closest method to execute' do
-          expect(A.new.concat('hello', ' world')).to eq 'hello world'
-        end
-
-        it 'should choose the correct and closest method to execute' do
-          expect(A.new.concat('hello', 3) ).to eq 'hellohellohello'
-        end
-
-        it 'should choose the correct and closest method to execute' do
-          expect(A.new.concat(['hello', ' world', '!'])).to eq 'hello world!'
-        end
-
-        it 'should choose the correct and closest method to execute' do
-          expect(A.new.concat(Object.new, 3)).to eq 'Objetos concatenados'
-        end
-
-
-        it 'should throw error if no compatible definition is available' do
-          expect{A.new.concat('hello', 'world', '!')}.to raise_error ArgumentError
-        end
-
-      end
-
-      describe 'Asking for the multimethods' do
-
-        it 'should return one symbol representing the multimethod' do
-          expect(A.multimethods).to eq [:respond_to?,:concat]
-        end
-        it 'should return the array of partialblocks which represents the multimethod' do
-          expect(A.multimethod(:concat).is_a? Array).to be true
-        end
-        it 'should be true that all of the elements on the array are partialblocks' do
-          expect(A.multimethod(:concat).all? do |mm| mm.is_a? PartialBlock end).to be true
-        end
-      end
-
-      describe 'respond_to?' do
-
-      it 'should respond if the method is a known multimethod' do
-        expect(A.new.respond_to?(:concat)).to be true
-       end
-
-      it 'should respond if the method is a known regular method' do
-        expect(A.new.respond_to?(:to_s)).to be true
-      end
-
-      it 'should respond if the method is a known multimethod and the types are correct' do
-        expect(A.new.respond_to?(:concat, false, [String,String])).to be true
-      end
-
-      it 'should respond if the method is a known multimethod and the types are correct' do
-        expect(A.new.respond_to?(:concat, false, [Integer,A])).to be true
-      end
-
-      it 'should not respond if the method is a regular method and types are provided' do
-        expect(A.new.respond_to?(:to_s, false, [String]) ).to be false
-      end
-
-      it 'should not respond if the method is a multimethod but the types are not correct' do
-        expect(A.new.respond_to?(:concat, false, [String,String,String])).to be false
-      end
-
-    end
 
   end
 
@@ -117,72 +131,7 @@ describe 'MultiMethods' do
       end
     end
 
-    describe 'Calling the methods' do
-
-
-      it 'should choose the correct and closest method to execute' do
-        expect(my_object.concat('hello', ' world')).to eq 'hello world'
-      end
-
-      it 'should choose the correct and closest method to execute' do
-        expect(my_object.concat('hello', 3) ).to eq 'hellohellohello'
-      end
-
-      it 'should choose the correct and closest method to execute' do
-        expect(my_object.concat(['hello', ' world', '!'])).to eq 'hello world!'
-      end
-
-      it 'should choose the correct and closest method to execute' do
-        expect(my_object.concat(Object.new, 3)).to eq 'Objetos concatenados'
-      end
-
-
-      it 'should throw error if no compatible definition is available' do
-        expect{my_object.concat('hello', 'world', '!')}.to raise_error ArgumentError
-      end
-
-    end
-
-    describe 'Asking for the multimethods' do
-
-      it 'should return one symbol representing the multimethod' do
-        expect(my_object.singleton_class.multimethods).to eq [:respond_to?,:concat]
-      end
-      it 'should return the array of partialblocks which represents the multimethod' do
-        expect(my_object.singleton_class.multimethod(:concat).is_a? Array).to be true
-      end
-      it 'should be true that all of the elements on the array are partialblocks' do
-        expect(my_object.singleton_class.multimethod(:concat).all? do |mm| mm.is_a? PartialBlock end).to be true
-      end
-    end
-
-    describe 'respond_to?' do
-
-      it 'should respond if the method is a known multimethod' do
-        expect(my_object.respond_to?(:concat)).to be true
-      end
-
-      it 'should respond if the method is a known regular method' do
-        expect(my_object.respond_to?(:to_s)).to be true
-      end
-
-      it 'should respond if the method is a known multimethod and the types are correct' do
-        expect(my_object.respond_to?(:concat, false, [String,String])).to be true
-      end
-
-      it 'should respond if the method is a known multimethod and the types are correct' do
-        expect(my_object.respond_to?(:concat, false, [Integer,A])).to be true
-      end
-
-      it 'should not respond if the method is a regular method and types are provided' do
-        expect(my_object.respond_to?(:to_s, false, [String]) ).to be false
-      end
-
-      it 'should not respond if the method is a multimethod but the types are not correct' do
-        expect(my_object.respond_to?(:concat, false, [String,String,String])).to be false
-      end
-
-    end
+    include_context 'Multimethods Behavior',my_object,my_object.singleton_class,[:respond_to?,:concat]
 
 
   end
@@ -211,51 +160,26 @@ describe 'MultiMethods' do
     end
     class B < A
       partial_def(:b_method,[]) do "I am B" end
+      partial_def(:b_method,[String]) do |nom| "I am #{nom}" end
       partial_def(:concat, [Array,String]) do |a,b| "#{a.join(', ')} y #{b}"end
     end
 
-    describe 'Calling the methods' do
+    include_context 'Multimethods Behavior',B.new,B,[:respond_to?,:concat,:b_method]
 
+    describe 'Multimethods of a subclass specific behavior' do
 
-      it 'should choose the correct and closest method to execute' do
-        expect(B.new.concat('hello', ' world')).to eq 'hello world'
-      end
-
-      it 'should choose the correct and closest method to execute' do
-        expect(B.new.concat('hello', 3) ).to eq 'hellohellohello'
-      end
-
-      it 'should choose the correct and closest method to execute' do
-        expect(B.new.concat(['hello', ' world', '!'])).to eq 'hello world!'
-      end
-
-      it 'should choose the correct and closest method to execute' do
-        expect(B.new.concat(Object.new, 3)).to eq 'Objetos concatenados'
-      end
+      describe 'Calling the methods' do
 
       it 'should choose the correct and closest method to execute' do
         expect(B.new.concat(['horacio','la maga'], 'rocamadour')).to eq 'horacio, la maga y rocamadour'
       end
 
-      it 'should throw error if no compatible definition is available' do
-        expect{B.new.concat('hello', 'world', '!')}.to raise_error ArgumentError
-      end
-
     end
 
-    describe 'Asking for the multimethods' do
+      describe 'Asking for the multimethods' do
 
-      it 'should return one symbol representing the multimethod' do
-        expect(B.multimethods).to include :respond_to?, :concat, :b_method
-      end
-      it 'should return the array of partialblocks which represents the multimethod' do
-        expect(B.multimethod(:concat).is_a? Array).to be true
-      end
       it 'should return the array of partialblocks which represents the multimethod' do
         expect(B.multimethod(:b_method).is_a? Array).to be true
-      end
-      it 'should be true that all of the elements on the array are partialblocks' do
-        expect(B.multimethod(:concat).all? do |mm| mm.is_a? PartialBlock end).to be true
       end
       it 'should be true that all of the elements on the array are partialblocks' do
         expect(B.multimethod(:b_method).all? do |mm| mm.is_a? PartialBlock end).to be true
@@ -263,31 +187,21 @@ describe 'MultiMethods' do
 
     end
 
-    describe 'respond_to?' do
+      describe 'respond_to?' do
 
       it 'should respond if the method is a known multimethod' do
-        expect(B.new.respond_to?(:concat)).to be true
+        expect(B.new.respond_to?(:b_method)).to be true
       end
 
-      it 'should respond if the method is a known regular method' do
-        expect(B.new.respond_to?(:to_s)).to be true
+      it 'should respond if the method is a known multimethod and the types match' do
+        expect(B.new.respond_to?(:b_method,false,[])).to be true
       end
 
-      it 'should respond if the method is a known multimethod and the types are correct' do
-        expect(B.new.respond_to?(:concat, false, [String,String])).to be true
+      it 'should respond if the method is a known multimethod and the types match' do
+        expect(B.new.respond_to?(:b_method,false,[String])).to be true
       end
 
-      it 'should respond if the method is a known multimethod and the types are correct' do
-        expect(B.new.respond_to?(:concat, false, [Integer,A])).to be true
-      end
-
-      it 'should not respond if the method is a regular method and types are provided' do
-        expect(B.new.respond_to?(:to_s, false, [String]) ).to be false
-      end
-
-      it 'should not respond if the method is a multimethod but the types are not correct' do
-        expect(B.new.respond_to?(:concat, false, [String,String,String])).to be false
-      end
+    end
 
     end
 
